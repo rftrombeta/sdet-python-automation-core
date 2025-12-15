a5440e5d16a5838695365b322ca46bcc2bce4701
Rodrigo Trombeta <rftrombeta@gmail.com>
2025-12-14 21:35:54 -0300

    Update

--- src/sdet_python_automation_core/core/context/execution_context.py
+++
@@ -1,6 +0,0 @@
-# src/sdet_python_automation_core/core/context/execution_context.py
-
-class ExecutionContext:
-    def __init__(self):
-        self.http_client = None
-        self.last_response = None
--- src/util/__init__.py
+++
@@ -1,1 +0,0 @@
-from utils import (force_known_error_message)
